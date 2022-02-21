c5705ac5d7c2e3213532028460b53aa162c67567
Vit Brunner <vit.brunner@gmail.com>
2022-02-21 21:20:00 +0100

    Web server

---
+++ roles/web-server/files/html-index.php
@@ -0,0 +1,12 @@
+<?php
+header("HTTP/1.0 404 Not Found");
+?>
+<html>
+<head>
+<title>404</title>
+</head>
+<body>
+<h1>¯\_(ツ)_/¯</h1>
+<p>Perhaps visit <a href="https://tasuki.org">tasuki.org</a> or go read a book, I dunno.</p>
+</body>
+</html>
