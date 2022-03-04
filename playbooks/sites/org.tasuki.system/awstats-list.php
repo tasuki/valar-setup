bc45e18cb8dc58c157ac24d4a1b8afdd7ba3df0f
Vit Brunner <vit.brunner@gmail.com>
2022-03-04 17:45:49 +0100

    Show uniques per month in awstats list

--- playbooks/sites/org.tasuki.system/awstats-list.php
+++ playbooks/sites/org.tasuki.system/awstats-list.php
@@ -4,6 +4,14 @@
 foreach (scandir("/etc/awstats") as $file) {
   if (preg_match("/^awstats\.(.*)\.conf$/", $file, $matches)) {
     $config = $matches[1];
-    echo "<h3><a href='/awstats/?config=$config'>$config</a></h3>";
+    $month = date("mY");
+    $content = file_get_contents("/var/lib/awstats/awstats$month.$config.txt");
+    if (preg_match("/^TotalUnique (.*)\$/m", $content, $matches)) {
+      $uniques = $matches[1];
+    } else {
+      $uniques = "unknown";
+    }
+    echo "<h3><a href='/awstats/?config=$config'>$config</a> " .
+         "($uniques uniq in " . date('M') . ")</h3>";
   }
 }
